c5d0c5ddc35a1ee9d9e7dbb84d96b8032f1d8214
Nichole Boothroyd <nboothro@ucsc.edu>
2019-07-22 03:36:44 -0700

    Walls enabled

---
+++ Roll-A-Ball/Assets/Scripts/cameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;   
+    }
+
+    // Runs after all other updates have been performed. Ensures we get final player position
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Roll-A-Ball/Assets/Scripts/playerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //FixedUpdate called before any physics update. Since movement is used with
+    //physics, script will use FixedUpdate
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+
+
+        rb.AddForce(movement * speed);
+    }
+}
